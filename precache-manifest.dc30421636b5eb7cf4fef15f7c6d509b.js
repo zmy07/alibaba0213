252b888db6b963e9ef1da66bd6ec9f23c20fd442
zmy07 <zhumy07@163.com>
2020-02-13 20:43:54 +0800

    Updates

---
+++ precache-manifest.dc30421636b5eb7cf4fef15f7c6d509b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45845c73a8616924c3de3699e9a0f66",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bec07e473d9d0aa28a4",
+    "url": "./static/css/main.1bc26e9b.chunk.css"
+  },
+  {
+    "revision": "16c00627d6d4f5ac57b2",
+    "url": "./static/js/2.4a344c4c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.4a344c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bec07e473d9d0aa28a4",
+    "url": "./static/js/main.919fd9d7.chunk.js"
+  },
+  {
+    "revision": "9c01993eb9ad08e24e93",
+    "url": "./static/js/runtime-main.489a2726.js"
+  }
+]);
